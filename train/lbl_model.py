655b1059d8499fe629389f8ea817df922c642bdf
Shib Dasgupta <ssdasgupta@umass.edu>
2021-01-12 20:06:50 -0800

    diagonal context matrix added for lbl

--- train/lbl_model.py
+++ train/lbl_model.py
@@ -24,9 +24,12 @@ parser.add_argument('--lr', type=float, default=20, help='initial learning rate'
 parser.add_argument('--n_gram',type=int, default=4, help='Number of previous words to consider')
 parser.add_argument('--embedding_dim', type=int, default=50, help='Word embedding dimensions')
 parser.add_argument('--num_epochs', type=int, default=50, help='Number of training epoch')
+parser.add_argument('--sep_output', type=bool, default=False, help='Use seperate ouput variable')
+parser.add_argument('--diag_context', type=bool, default=False, help='Use seperate ouput variable')
+
 args = parser.parse_args()
 
-wandb.init(project="lbl-language-model",  reinit=True)
+wandb.init(project="box-language-model",  reinit=True)
 wandb.config.update(args)
 
 class LBLModel(nn.Module):
@@ -43,17 +46,29 @@ class LBLModel(nn.Module):
         self.embeddings_word = nn.Embedding(self.vocab_size, self.embedding_dim)
         self.embedding_bias = nn.Embedding(self.vocab_size, 1)
         self.embedding_bias.weight.data = torch.zeros(self.vocab_size, 1)
-
-        self.C = nn.Embedding(n_gram, self.embedding_dim * self.embedding_dim)
+        if args.sep_output:
+            self.embeddings_word_output = nn.Embedding(self.vocab_size, self.embedding_dim)
+        if args.diag_context:
+            self.C = nn.Embedding(n_gram, self.embedding_dim)
+        else:
+            self.C = nn.Embedding(n_gram, self.embedding_dim * self.embedding_dim)
     
     def forward(self, x, train = True):
         """ predict, return hidden state so it can be used to intialize the next hidden state """
         context_word_features = self.embeddings_word(x)
         all_gram_idx = torch.arange(self.n_gram).cuda() if use_cuda else torch.arange(self.n_gram)
         all_vocab_idx = torch.arange(self.vocab_size).cuda() if use_cuda else torch.arange(self.vocab_size)
-        position_matrix = self.C(all_gram_idx).reshape(-1, self.embedding_dim, self.embedding_dim)
-        context_features = torch.tensordot(context_word_features, position_matrix)
-        all_word = self.embeddings_word(all_vocab_idx)
+        position_matrix = self.C(all_gram_idx)
+        if not args.diag_context:
+            position_matrix = position_matrix.reshape(-1, self.embedding_dim, self.embedding_dim)
+            context_features = torch.tensordot(context_word_features, position_matrix)
+        else:
+            context_features = torch.mean(context_word_features * position_matrix , dim=1)
+
+        if args.sep_output:
+            all_word = self.embeddings_word_output(all_vocab_idx)
+        else:
+            all_word = self.embeddings_word(all_vocab_idx)
         decoded = torch.mm(context_features,  all_word.T) + self.embedding_bias(all_vocab_idx).view(-1)
         logits = F.log_softmax(decoded, dim = 1)       
         return logits
